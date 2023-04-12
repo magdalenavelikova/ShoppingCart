b80fa9fab050c55a4f947073f24d7f438f4569e7
MLVelikova <magibox@yahoo.com>
2023-04-12 10:22:43 +0300

    push

---
+++ shopping-cart/src/App.jsx
@@ -0,0 +1,16 @@
+//import logo from './logo.svg';
+import './App.css';
+
+import ShoppingCart from "./components/ShoppingCart/ShoppingCart";
+
+//import ShoppingCart from './components/ShoppingCart/ShoppingCart';
+
+function App() {
+  return (
+    <div className="App">
+   <ShoppingCart/>
+    </div>
+  );
+}
+
+export default App;
